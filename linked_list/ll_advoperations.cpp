6372f7306db65f91a8c3320d955a6fc6de83bf67
Srikarthick <srikarthi01234@gmail.com>
2021-10-26 19:53:01 +0530

    added linkedlist_operation

--- linked_list/ll_advoperations.cpp
+++ linked_list/ll_advoperations.cpp
@@ -56,21 +56,86 @@ void insertBeg()
 {
 	NODE *currptr;
 	currptr = new NODE;
-	cout<<"Enter the info of new node: ";
+	cout<<"Enter the info of new NODE: ";
 	cin>> currptr->info;
 	currptr->link = head;
 	head = currptr;
 }
+
+void insertEnd(NODE *p)
+{
+	NODE *newnode;
+	while(p->link != NULL)
+	{
+		p = p->link;
+	}
+	newnode = new NODE;
+	cout<<"Enter the info of the new NODE: ";
+	cin>>newnode->info;
+	p->link = newnode;
+	newnode->link = NULL;
+		
+}
+
+void insertPos(NODE *p, int pos)
+{
+	NODE *currptr = head, *newnode;
+	if(pos==1)
+	{
+		insertBeg();
+	}
+	else
+	{
+		for(int i=0; i<pos-2; i++)
+			currptr = currptr->link;
+		newnode = new NODE;
+		cout<<"\nEnter the item of the NODE: ";
+		cin>>newnode->info;
+		newnode->link = currptr->link;
+		currptr->link = newnode;
+	}
+}
+
+void deleteBeg()
+{
+	NODE *currptr;
+	if(head==NULL)
+		cout<<"Linked List is Empty!!!!";
+	else
+	{
+		currptr = head;
+		head = head->link;
+		free(currptr);
+	}
+}
+
+void deleteEnd()
+{
+	NODE *currptr = head, *p;
+		while(currptr->link!=NULL)
+		{
+			currptr = currptr->link;
+		}
+		p = currptr;
+		p->link = NULL;
+}
+
 int main()
 {
-	int choice;
+	int choice, pos, posDel;
 	while(1)
 	{
 	
 	cout<<"\n1. Create Linked list: ";
 	cout<<"\n2. Display Linked List: ";
 	cout<<"\n3. Insert a Node at beginning of Linked List: ";
-	cout<<"\n4. Exit!";
+	cout<<"\n4. Insert a Node at End of the Linked List: ";
+	cout<<"\n5. Insert a Node at desired position in the List: ";
+	cout<<"\n6. Delete a Node at beginning of the List?";
+	cout<<"\n7. Delete a Node at the End of the List?";
+	cout<<"\n8. Delete a Node at the desired position in the List: ";
+	cout<<"\n9. Search for a Node in the list: ";
+	cout<<"\n10. Exit!";
 	cout<<"\n\nEnter Your Choice: ";
 	cin>> choice;
 	switch(choice)
@@ -78,10 +143,20 @@ int main()
 		case 1: createsinglinklist(); break;
 		case 2: displaylinklist(head); break;
 		case 3: insertBeg(); break;
-		case 4: exit(0);
+		case 4: insertEnd(head); break;
+		case 5: cout<<"\nEnter the position of the node: ";
+				cin>>pos;
+				insertPos(head, pos); break;
+		case 6: deleteBeg(); break;
+		case 7: deleteEnd(); break;
+		// case 8: cout<<"Enter the position at which the Node has to be deleted: ";
+				// cin>>posDel;
+				// deletePos(posDel); break;
+		case 9: cout<<"Enter the info that has to be searched: ";
+		case 10: exit(0);
 		default: cout<<"Wrong choice!!!"; break;
 		}
 
-	//22
+	//2
 	}
 }
